db4eee87a304b0597d5c5eadabc26b5b7ca8a2ba
Isuru Munasinghe <87745533+IsuruMunasinghe98@users.noreply.github.com>
2023-02-22 23:13:03 +0530

    Update Arbiter.v
    Adding slave selection states by the master(Priority of master 1 >  master 2)

--- src/Arbiter.v
+++ src/Arbiter.v
@@ -39,12 +39,13 @@ output reg[1:0] slave_sel
 );
 
 parameter[2:0] IDLE_STATE = 3'd0;
-parameter[2:0] MASTER1_REQUEST_STATE = 3'd1;
-parameter[2:0] MASTER2_REQUEST_STATE  = 3'd2;
-
+parameter[2:0] MASTER1_SLAVE_SELECT_STATE = 3'd1;
+parameter[2:0] MASTER2_SLAVE_SELECT_STATE  = 3'd2;
+parameter SEL0=0, SEL1=1;
 
 
 reg [2:0] state = IDLE_STATE;
+reg [1:0] SEL_STATE = SEL0;
 
 always@(posedge clk or posedge rst)
 begin
@@ -65,19 +66,19 @@ begin
             arbiter_busy <= 1;
             bus_grant <= 2'd1;
             slave_sel  <= 2'd0;
-            state <= MASTER1_REQUEST_STATE;
+            state <= MASTER1_SLAVE_SELECT_STATE;
          end
          
     // if master 2 request and master 1 is not acquiring the bus and arbiter is not busy
     // make the next state as MASTER2_REQUEST_STATE      
-    else if((m2_request == 1) && (state != MASTER1_REQUEST_STATE) && (arbiter_busy != 1))
+    else if((m2_request == 1) && (state != MASTER1_SLAVE_SELECT_STATE) && (arbiter_busy != 1))
         begin
             m1_grant <= 0;
             m2_grant <= 1;
             arbiter_busy <= 1;
             bus_grant <= 2'd2;
             slave_sel  <= 2'd0;
-            state <= MASTER2_REQUEST_STATE;
+            state <= MASTER2_SLAVE_SELECT_STATE;
         end
      else
         begin
@@ -94,15 +95,48 @@ end
 always @(posedge clk) 
     begin
         case(state)
-        MASTER1_REQUEST_STATE :begin
-        
-        
-        end
-        
+        MASTER1_SLAVE_SELECT_STATE:
+            begin
+                case(SEL_STATE)
+                    SEL0:
+                        begin
+                        slave_sel[0]<=m1_slave_sel;
+                        SEL_STATE<=SEL1;
+                        end
+                    SEL1:
+                        begin
+                        slave_sel[1]<=m1_slave_sel;
+                        SEL_STATE<=SEL0;
+                        state<=IDLE_STATE;
+                        end
+                 endcase
+             end
+             
+         MASTER2_SLAVE_SELECT_STATE:
+             begin
+                 case(SEL_STATE)
+                     SEL0:
+                         begin
+                         slave_sel[0]<=m2_slave_sel;
+                         SEL_STATE<=SEL1;
+                         end
+                     SEL1:
+                         begin
+                         slave_sel[1]<=m2_slave_sel;
+                         SEL_STATE<=SEL0;
+                         state<=IDLE_STATE;
+                         end
+                  endcase
+              end
+              
+           IDLE_STATE:
+               begin
+                   slave_sel = 2'b0;
+                   arbiter_busy <= 0;   
+                   
+               end
         endcase
 
     end
-    
-
 
 endmodule
