52f4f8263b9421d6e3c2da9e3fd731291f7bcefb
VishvaOshada <180665p@uom.lk>
2023-03-20 23:34:52 +0530

    Debugged Slave

--- src/slave.v
+++ src/slave.v
@@ -24,8 +24,6 @@ module slave(
 	input clk, 
 	input reset,
 
-	//input [5:0]slave_delay,
-
 	input read_en,
 	input write_en,
 
@@ -39,50 +37,49 @@ module slave(
 	input rx_data,
 	input rx_burst,
 
-	// output slave_tx_done,
-	// output rx_done,
-	output tx_data/*,
-	output split_en*/);
+	output tx_data);
 	
 	
-    wire [7:0]datain;
-    wire [11:0]address;
-    wire [7:0]data;
-    wire read_en_in;
-    wire write_en_in;
+    wire [7:0]data_in;
+    wire [11:0]addr;
+    wire [7:0]data_out;
+    wire read_enable;
+    wire write_enable;
     wire reset_busy;
     
     slave_port SLAVE_PORT(
         .clk(clk), 
         .reset(reset),
-        //.slave_delay(slave_delay),
+        
         .read_en(read_en),
         .write_en(write_en),
+        
         .master_ready(master_ready),
         .master_valid(master_valid),
+        
         .slave_valid(slave_valid),
         .slave_ready(slave_ready),
+        
         .rx_addr(rx_addr),
         .rx_data(rx_data),
         .rx_burst(rx_burst),
-        // .slave_tx_done(slave_tx_done),
-        // .rx_done(rx_done),
         .tx_data(tx_data),
-        .datain(datain),
-        .address(address),
-        .data(data),
-        .read_en_in(read_en_in),
-        .write_en_in(write_en_in)/*,
-        .split_en(split_en)*/);
         
-        bram bram_wrapper(
-        .BRAM_PORTA_0_addr(address),
+        .data_in(data_in),
+        .addr_out(addr),
+        .data_out(data_out),
+        
+        .read_enable(read_enable),
+        .write_enable(write_enable));
+        
+        bram_wrapper bram(
+        .BRAM_PORTA_0_addr(addr),
         .BRAM_PORTA_0_clk(clk),
-        .BRAM_PORTA_0_din(datain),
-        .BRAM_PORTA_0_dout(data),
-        .BRAM_PORTA_0_en(read_en_in),
+        .BRAM_PORTA_0_din(data_out),
+        .BRAM_PORTA_0_dout(data_in),
+        .BRAM_PORTA_0_en(read_enable),
         .BRAM_PORTA_0_rst(reset),
-        .BRAM_PORTA_0_we(write_en_in),
+        .BRAM_PORTA_0_we(write_enable),
         .rsta_busy_0(reset_busy));
         
 endmodule 
